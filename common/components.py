498073823f10af95208e9fa29356ce4e7d714bd9
Tess Ellenoir Duursma <tess@vanu.dev>
2024-11-29 18:58:34 +0100

    Replace Element with Node

--- common/components.py
+++ common/components.py
@@ -1,12 +1,10 @@
 from django.templatetags.static import static
 from django.urls import reverse
-from htpy import Element, HTMLElement, a, body, div, h1, h2, head, html, link, main
+from htpy import HTMLElement, Node, a, body, div, h1, h2, head, html, link, main
 
 
-def wrapper(
-    *children: Element, sub_heading: Element | str | None = None
-) -> HTMLElement:
-    sub_heading_component: None | Element = None
+def wrapper(*children: Node, sub_heading: Node = None) -> HTMLElement:
+    sub_heading_component: None | Node = None
     if sub_heading is not None:
         sub_heading_component = h2[sub_heading]
 
