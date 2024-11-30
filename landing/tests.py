551461260309bb4f30acb45d16b8b4fa6af3578b
VanuPhantom <tess@vanu.dev>
2024-12-01 00:44:36 +0100

    Add tests for the landing page

--- landing/tests.py
+++ landing/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.urls import reverse
+
+from common.utils import HtmxTestCase
+
 
 # Create your tests here.
+class LandingPageTests(HtmxTestCase):
+    def test_htmx_headers_give_partial(self):
+        response = self.client.get(
+            reverse("landing:index"), headers={"HX-Request": "true"}
+        )
+
+        self.assertIsPartial(response)
+
+    def test_no_htmx_headers_give_full(self):
+        response = self.client.get(reverse("landing:index"))
+
+        self.assertIsNotPartial(response)
