a196de2615120876d55a1a00c3356fd7625aa878
VanuPhantom <tess@vanu.dev>
2024-12-01 00:08:53 +0100

    Add integration testing for the blog post viewer

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,9 @@
 from datetime import date
+from typing import cast
 
 from django.test import TestCase
 from django.urls import reverse
+from pyquery import PyQuery
 
 from .models import Post
 
@@ -22,3 +24,39 @@ class PostTests(TestCase):
         url = reverse("blog:post", args=[post.pk])
 
         self.assertEqual(post.url, url)
+
+
+class PostViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.bulk_create(
+            [Post(title=f"Post {n}", content="Nyaaa *paws* :3") for n in range(100)]
+        )
+
+    def test_existing_post_gives_200(self):
+        post = cast(Post, Post.objects.first())
+
+        response = self.client.get(reverse("blog:post", args=[post.pk]))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_non_existent_id_gives_404(self):
+        response = self.client.get(reverse("blog:post", args=[300]))
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_htmx_headers_give_partial(self):
+        post = cast(Post, Post.objects.first())
+
+        response = self.client.get(
+            reverse("blog:post", args=[post.pk]), headers={"HX-Request": "true"}
+        )
+
+        self.assertFalse(PyQuery(response.content).is_("html"))
+
+    def test_no_htmx_headers_give_full(self):
+        post = cast(Post, Post.objects.first())
+
+        response = self.client.get(reverse("blog:post", args=[post.pk]))
+
+        self.assertTrue(PyQuery(response.content).is_("html"))
