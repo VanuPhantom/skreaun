1495c9bb05ce549aaa1bc811c925132267eeb1ac
VanuPhantom <tess@vanu.dev>
2024-11-30 23:43:08 +0100

    Add unit testing for the Post model

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+
+class PostTests(TestCase):
+    def test_stringify_returns_title(self):
+        post = Post(title="Test", content="Hello, world")
+        self.assertEqual(post.title, str(post))
+
+    def test_default_data_is_today(self):
+        post = Post(title="Test", content="Hello, world")
+        self.assertEqual(post.date, date.today())
+
+    def test_url_points_to_post(self):
+        post = Post(title="Test", content="Hello, world")
+        post.save()
+
+        url = reverse("blog:post", args=[post.pk])
+
+        self.assertEqual(post.url, url)
